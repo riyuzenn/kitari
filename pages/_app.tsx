a59d7eb04ea0d9680fc25d899b80c8fda54868a8
riyuzenn <riyuzenn@gmail.com>
2022-08-30 09:32:20 +0800

    feat: create top progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 import "swiper/css";
+import NextNProgress from "nextjs-progressbar";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 
 function Kitari({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider attribute="class" defaultTheme="dark">
+            <NextNProgress color="#937193" />
             <Component {...pageProps} />
         </ThemeProvider>
     );
